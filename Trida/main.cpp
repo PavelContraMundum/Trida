a6c56520ef82fa9a8d34e232b1078ba7f8fed1c6
Pavel Štička <pavelsticka@Pavel--Mac-mini.local>
2024-07-27 21:25:51 +0200

    pridana virtualni fce a derivovane tridy

--- Trida/main.cpp
+++ Trida/main.cpp
@@ -3,6 +3,16 @@
 #include <vector>
 #include <algorithm>
 
+
+class Person {
+public:
+    virtual void getDescription() const {
+        std::cout << "Toto je osoba." << std::endl;
+    }
+    virtual ~Person() = default; // Virtuální destruktor
+};
+
+
 class Class {
 private:
     std::string name;
@@ -26,7 +36,24 @@ public:
     }
 };
 
-class Student {
+
+class Ucitel : public Person {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string subject;
+
+public:
+    Ucitel(const std::string& fName, const std::string& lName, const std::string& subj)
+        : firstName(fName), lastName(lName), subject(subj) {}
+
+    void getDescription() const override {
+        std::cout << "Jméno: " << firstName << " " << lastName << ", Předmět: " << subject << std::endl;
+    }
+};
+
+
+class Student : public Person {
 private:
     std::string firstName;
     std::string lastName;
@@ -49,7 +76,14 @@ public:
     std::string getClassName() const {
         return studentClass.getName();
     }
+    
+    void getDescription() const override {
+           std::cout << "Jméno: " << firstName << " " << lastName << ", Věk: " << age
+                     << ", Třída: " << studentClass.getName() << std::endl;
+       }
 };
+
+
 
 std::vector<Student> filterStudentsByClass(const std::vector<Student>& students, const std::string& className) {
     std::vector<Student> filteredStudents;
@@ -66,23 +100,38 @@ int main() {
     Class class1B("1B", 1);
     Class class2A("2A", 2);
 
-    std::vector<Student> students;
-    students.emplace_back("Jan", "Novák", 16, class1A);
-    students.emplace_back("Petra", "Svobodová", 17, class1B);
-    students.emplace_back("Lukáš", "Král", 15, class2A);
-    students.emplace_back("Eva", "Kučerová", 16, class1A);
-    students.emplace_back("Petra", "Jirásková", 14, class1A);
+   // std::vector<Student> students;
+    std::vector<Person*> people;
+    people.push_back(new Person());
+    people.push_back(new Student("Jan", "Novák", 16, class1A));
+    people.push_back(new Student("Petra", "Svobodová", 17, class1B));
+    people.push_back(new Student("Lukáš", "Král", 15, class2A));
+    people.push_back(new Student("Eva", "Kučerová", 16, class1A));
+    people.push_back(new Ucitel("Martin", "Černý", "Matematika"));
+    people.push_back(new Ucitel("Jana", "Horáková", "Fyzika"));
 
-    std::cout << "Všichni studenti:\n";
-    for (const auto& student : students) {
-        student.displayInfo();
-    }
+    
 
-    std::cout << "\nStudenti ve třídě 1A:\n";
-    auto studentsIn1A = filterStudentsByClass(students, "1A");
-    for (const auto& student : studentsIn1A) {
-        student.displayInfo();
-    }
+   // std::cout << "Všichni studenti:\n";
+   // for (const auto& student : students) {
+   //     student.displayInfo();
+   // }
+
+   // std::cout << "\nStudenti ve třídě 1A:\n";
+   // auto studentsIn1A = filterStudentsByClass(students, "1A");
+   // for (const auto& student : studentsIn1A) {
+   //     student.displayInfo();
+  //  }
+    
+    for (const auto& person : people) {
+            person->getDescription();
+        }
+
+        // Uvolnění paměti
+        for (auto& person : people) {
+            delete person;
+        }
+    
 
     return 0;
 }
